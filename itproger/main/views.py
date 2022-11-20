56c4c67cb9d6dbbf01dbaec7288588db3730cc03
Rail_Akhm <railakhmetshin@gmail.com>
2022-11-20 17:32:10 +0500

    Initial commit

---
+++ itproger/main/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    data = {
+        'title': 'Главная страница!!',
+    }
+    return render(request, 'main/index.html', data)
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def contacts(request):
+    return render(request, 'main/contacts.html')
